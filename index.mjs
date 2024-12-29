f7b50ea9234429e40dcfecf424bf3e076c9add7f
Muhammad saad <saadrahimit789@gmail.com>
2024-12-29 05:03:36 -0800

    add cors

--- index.mjs
+++ index.mjs
@@ -1,14 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import { products } from './productData.mjs'
 
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req, response) => {
     response.send('Hello World from FashionCart!')
   })
-
-
 
 app.get('/product', (req, response) => {
 
